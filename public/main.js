47fe2cae7b7a1f1f59b13c70b8b56695906d87c8
Johny Ho <jxho1@cs.washington.edu>
2023-11-27 19:01:16 -0800

    prototype

--- public/main.js
+++ public/main.js
@@ -1,1 +1,8 @@
-console.log("Add your visualizations here!");
+document.addEventListener('DOMContentLoaded', () => {
+    const map = L.map('map-container').setView([51.505, -0.09], 2);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+  });
